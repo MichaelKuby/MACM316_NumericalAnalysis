8912eca8aa88ac66f3f497dd06018cae91fef692
Michael Kuby <105996614+MichaelKuby@users.noreply.github.com>
2023-01-27 17:59:25 -0800

    update

--- MACM 316/Assignment #1/Assignment_1_Script.m
+++ MACM 316/Assignment #1/Assignment_1_Script.m
@@ -2,41 +2,29 @@ clc
 clear
 format long
 
-inputs = linspace(0, 2, 1000)
-outputs = zeros(size(inputs))
-ouputs_minusL = zeros(size(inputs))
-xSq = zeros(size(inputs))
-xCu = zeros(size(inputs))
+inputs = linspace(0, 1, 1000000);
+outputs = zeros(size(inputs));
+error = zeros(size(inputs));
 
 for i = 1:length(inputs)
     % Compute values for F(h) as h -> 0
-    outputs(i) = func(inputs(i))
+    outputs(i) = func(inputs(i));
     % Compute values for |F(h) - L| as h -> 0
-    outputs_minusL(i) = abs(outputs(i) - 2)
-    % Create the function y = x^2 to compare against
-    xSq(i) = (inputs(i)^2)
-    % Create the function y = x^3 to compare against
-    xCu(i) = (inputs(i)^3)
+    error(i) = abs(outputs(i) - 2);
 end
 
+%%
 % Plot the graph for F(h) =  ( e^h - e^{-h} ) / h
 plot(inputs, outputs, "LineWidth", 2)
 title("F(h) =  ( e^h - e^{-h} ) / h")
-ylabel("Output Values")
+ylabel("F(h)")
 yline(2)
-xlabel("Input Values")
+xlabel("h")
 xline(0)
 
-% Plot the graph for | ( e^h - e^{-h}  / h ) - L | and compare against x^2,
-% x^3, etc...
-plot(inputs, outputs_minusL, "LineWidth", 2)
-title("| ( e^h - e^{-h}  / h ) - L |")
-ylabel("Output Values")
-xlabel("Input Values")
-xline(0)
-
-hold on
-plot(inputs, xSq, '-b', "Color", "cyan")
-plot(inputs, xCu, '-b', "Color", "green")
-
-legend("| F(h) - L |", "h^2", "h^3")
+%%
+% Plot the log of h and the error
+loglog(inputs, error)
+title("loglog of Error of h vs. h")
+ylabel("Error of h")
+xlabel("h")
